63440bc29b213333e3c61c4ace0f6ca13a6c0ac3
qsmjasahak <qsmjasahak@tip.edu.ph>
2023-02-20 09:28:40 +0000

    Add Student.py file to CPE028_Module4

---
+++ Student.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__ (self,name, age):
+        self.name = name
+        self.age = age
+    def __str__ (str):
+        return f"{self.name}{(self.age)}"
+
+p1 = Student("Steve Michael Joshua Sahak", 22)
+p2 = Student("Walter White", 52)
+print("Name: ", p1.name)
+print("Age: ", p1.age)
+print("Name: ", p2.name)
+print("Age: ", p2.age)
